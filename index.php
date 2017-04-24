2a1a519cf1a434d0d5f7d013afb2eb5dbe82a425
Maxim Rysevets <root@effcore.com>
2017-04-24 15:35:47 +0300

    sprint_263_13

--- index.php
+++ index.php
@@ -12,7 +12,8 @@ namespace effectivecore {
   const nl              = "\n";
   const br              = "<br/>";
 
-  require_once('system/module_core/classes/abstract_global--core.php');
-  core::init();
+  require_once('system/module_core/classes/abstract_global--events.php');
+  require_once('system/module_core/classes/abstract_global--events_module.php');
+  events_module::on_init();
 
 }
