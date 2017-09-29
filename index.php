faada672c5dd4d997b5e32be2a81c2693d20c014
Maxim Rysevets <root@effcore.com>
2017-09-29 14:31:28 +0300

    sprint_311_145_1

--- index.php
+++ index.php
@@ -16,8 +16,6 @@ namespace effectivecore {
   const dir_system      = __DIR__.'/system/';
   const nl              = "\n";
   const br              = "<br/>";
-  const phase_0         = 0;
-  const phase_1         = 1;
 
   require_once('system/module_core/backend/factory--gl.php');
   require_once('system/module_core/backend/factory--gl--timers.php');
