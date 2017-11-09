c821bbe26c7e01d455c83491edef1377725d9f28
Maxim Rysevets <root@effcore.com>
2017-11-09 18:13:05 +0300

    sprint_311_220

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ namespace effectivecore {
   require_once('system/module_core/backend/factory--gl--dynamic.php');
   require_once('system/module_core/backend/factory--gl--cache.php');
   require_once('system/module_core/backend/class--gl--file.php');
-  require_once('system/module_core/backend/factory--gl--files.php');
+  require_once('system/module_core/backend/factory--gl--file.php');
   spl_autoload_register('\effectivecore\factory::autoload');
 
   use \effectivecore\url_factory as url;
