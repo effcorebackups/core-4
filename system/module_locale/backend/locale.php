dd79d0cfa4d6ba30633376b246b6daf4586d9d94
Maxim Rysevets <root@effcore.com>
2018-05-29 13:02:49 +0300

    sprint_312_246_120

---
+++ system/module_locale/backend/locale.php
@@ -0,0 +1,56 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2018 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore {
+          abstract class locale {
+
+  static protected $cache_settings;
+  static protected $cache_countries;
+
+  static function init() {
+    static::$cache_settings = storage::get('files')->select('settings/locales');
+    foreach (storage::get('files')->select('countries') as $c_module_id => $c_countries) {
+      foreach ($c_countries as $c_row_id => $c_country) {
+        if (isset(static::$cache_countries[$c_country->code])) console::add_log_about_duplicate('country', $c_country->code);
+        static::$cache_countries[$c_country->code] = $c_country;
+        static::$cache_countries[$c_country->code]->module_id = $c_module_id;
+      }
+    }
+  }
+
+  static function get_countries() {
+    if   (!static::$cache_countries) static::init();
+    return static::$cache_countries;
+  }
+
+  static function get_settings() {
+    if   (!static::$cache_settings) static::init();
+    return static::$cache_settings;
+  }
+
+  ###############
+  ### formats ###
+  ###############
+
+  static function format_time($time)                      {return \DateTime::createFromFormat('H:i:s',       $time,     new \DateTimeZone('UTC'))->setTimezone(new \DateTimeZone( static::get_settings()->timezone ))->format( static::get_settings()->format_time );}
+  static function format_date($date)                      {return \DateTime::createFromFormat('Y-m-d',       $date,     new \DateTimeZone('UTC'))->setTimezone(new \DateTimeZone( static::get_settings()->timezone ))->format( static::get_settings()->format_date );}
+  static function format_datetime($datetime)              {return \DateTime::createFromFormat('Y-m-d H:i:s', $datetime, new \DateTimeZone('UTC'))->setTimezone(new \DateTimeZone( static::get_settings()->timezone ))->format( static::get_settings()->format_datetime );}
+  static function format_timestamp($timestamp)            {return \DateTime::createFromFormat('U',           $timestamp                         )->setTimezone(new \DateTimeZone( static::get_settings()->timezone ))->format( static::get_settings()->format_datetime );}
+  static function format_persent($number, $precision = 2) {return static::format_number(floatval($number), $precision).'%';}
+  static function format_msecond($number, $precision = 6) {return static::format_number(floatval($number), $precision);}
+  static function format_version($number)                 {return static::format_number(floatval($number), 2);}
+
+  static function format_number($number, $precision = 0, $dec_point = null, $thousands = null, $no_zeros = true) {
+    $dec_point = is_null($dec_point) ? static::get_settings()->decimal_point       : $dec_point;
+    $thousands = is_null($thousands) ? static::get_settings()->thousands_separator : $thousands;
+    return core::format_number($number, $precision, $dec_point, $thousands, $no_zeros);
+  }
+
+  static function format_human_bytes($bytes, $decimals = 2) {
+    return core::bytes_to_human($bytes, $decimals, static::get_settings()->decimal_point);
+  }
+
+}}
